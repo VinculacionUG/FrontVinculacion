bf1419ba457c78f327ce7489ba441b4a0ec5a421
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 <vinculaciongrupofema@gmail.com>
2024-05-12 20:41:18 -0500

    Sellan_Leo_Main

--- frontvinculacion/src/components/SignIn.js
+++ frontvinculacion/src/components/SignIn.js
@@ -7,17 +7,21 @@ const SignIn = () => {
   const navigation = useNavigation();
   const [nombre, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
+  //const [showPassword, setShowPassword] = useState(false); // Nueva variable de estado
+  var id = document.getElementById('Contraseña');
+  var guaradarC = id.value
+  
   const handleSubmit = async () => {    
-    try{
-      const response = await fetch('https://www.fema.somee.com/Auth/login', {
+    try {
+      const response = await fetch('https://www.fema.somee.com/Auth/login',{
+      //const response = await fetch('http://localhost:7040/Auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
           "nombre": nombre,
-          "password": password,
+          "password": guaradarC,
         }),
       });
 
@@ -32,13 +36,18 @@ const SignIn = () => {
         }
       }
     } catch (error) {
-      console.log('Error al procesar la solicitud:', error.message);
+      console.console('Error al procesar la solicitud:', error.message);
       Alert.alert('Error', 'Ha ocurrido un error al iniciar sesión. Por favor, inténtalo de nuevo más tarde.');
     }
   };
 
   return (
     <View style={styles.container}>
+      
+      <TouchableOpacity style={styles.goBackButton} onPress={() => navigation.goBack()}>
+        <MaterialCommunityIcons name="arrow-left" size={24} color="white" />
+      </TouchableOpacity>
+
       <MaterialIcons name="description" size={60} color="white" style={styles.icon} />
       <View style={styles.paper}>
         <View style={styles.headingContainer}>
@@ -49,7 +58,7 @@ const SignIn = () => {
             placeholder="Usuario"
             keyboardType="email-address"
             autoCapitalize="none"
-            style={styles.input}
+            style={[styles.input, { color: nombre ? 'black' : 'gray' }]}
             value={nombre}
             onChangeText={setEmail}
           />
@@ -57,10 +66,10 @@ const SignIn = () => {
             ¿Olvidaste tu nombre de usuario?
           </Text>
           <TextInput
-            placeholder="Password"
+          id='Contraseña'
+            placeholder="Constraseña"
             secureTextEntry
-            style={styles.input}
-            value={password}
+            style={[styles.input, { color: password ? 'black' : 'gray' }]}
             onChangeText={setPassword}
           />
           <Text style={styles.forgotLink} onPress={() => navigation.navigate('RecoveryPassword')}>
@@ -71,10 +80,6 @@ const SignIn = () => {
             <Text style={styles.buttonText}>Iniciar Sesión</Text>
           </TouchableOpacity>
         </View>
-        <TouchableOpacity style={[styles.goBackButton, styles.horizontalPadding]} onPress={() => navigation.goBack()}>
-          <MaterialCommunityIcons name="arrow-left" size={24} color="white" />
-          <Text style={styles.goBackButtonText}>Regresar</Text>
-        </TouchableOpacity>
       </View>
     </View>
   );
@@ -86,6 +91,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#001f3f',
+    position: 'relative',
   },
   icon: {
     marginBottom: 20,
@@ -110,22 +116,26 @@ const styles = StyleSheet.create({
     marginTop: 16,
   },
   input: {
+    //color: 'gray',
     height: 40,
     borderColor: 'gray',
     borderWidth: 1,
     marginBottom: 10,
     paddingHorizontal: 10,
+    borderRadius: 5,
   },
   forgotLink: {
     color: '#001f3f',
     textDecorationLine: 'underline',
-    marginTop: 5,
+    //marginTop: 2,
+    //marginEnd: 5,
+    marginBottom: 15,
   },
   button: {
     flexDirection: 'row',
     alignItems: 'center',
     backgroundColor: '#001f3f',
-    padding: 8,
+    padding: 10,
     borderRadius: 5,
     marginTop: 24,
     width: '100%',
@@ -134,7 +144,9 @@ const styles = StyleSheet.create({
   buttonText: {
     color: 'white',
     marginLeft: 10,
+    textAlign: 'center',
   },
+
   goBackButton: {
     flexDirection: 'row',
     alignItems: 'center',
@@ -152,9 +164,27 @@ const styles = StyleSheet.create({
   horizontalPadding: {
     paddingHorizontal: 20, // Ajusta este valor para cambiar el espacio horizontal del botón
   },
+  
+  goBackButton: {
+    position: 'absolute',
+    top: 20,
+    left: 20,
+    zIndex: 1, // Asegura que la flecha esté sobre otros elementos
+  },
+
+  passwordContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  passwordInput: {
+    //marginBottom: 10,
+    //marginTop:10,
+    flex: 1,
+  },
+  passwordVisibilityButton: {
+    paddingHorizontal: 10,
+  },
+
 });
 
 export default SignIn;
-
-
-
