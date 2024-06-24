6a51b65ad91f75851030b8f01f6de25a02596504
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 <vinculaciongrupofema@gmail.com>
2024-06-23 20:18:59 -0500

    add formulario fema 4

--- frontvinculacion/src/components/FormularioFema4.js
+++ frontvinculacion/src/components/FormularioFema4.js
@@ -109,116 +109,118 @@ const FormularioFema4 = ({ route, navigation }) => {
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
+      <Text style={styles.title}>Formulario FEMA P-154</Text>
+      <Text style={[styles.subtitle, styles.blackText, styles.centerText]}>Extensión de la revisión</Text>
 
-        <Text style={styles.title}>Formulario FEMA P-154</Text>
-        <Text style={styles.subtitle}>Extensión de la revisión</Text>
+      <View style={styles.row}>
+        <Text style={styles.inputLabel}>Exterior:</Text>
+        <View style={{ width: 65 }} /> 
+        <Picker
+          style={[styles.input, styles.picker]}
+          selectedValue={revisionExterior}
+          onValueChange={(itemValue) => setRevisionExterior(itemValue)}
+        >
+          <Picker.Item label="Parcial" value="Parcial" />
+          <Picker.Item label="Todos los Lados" value="Todos los Lados" />
+          <Picker.Item label="Aéreo" value="Aéreo" />
+        </Picker>
+      </View>
 
-        <View style={styles.inputContainer}>
-          <Text style={styles.inputLabel}>Exterior:  </Text>
-          <Picker
-            style={styles.input}
-            selectedValue={revisionExterior}
-            onValueChange={(itemValue) => setRevisionExterior(itemValue)}
-          >
-            <Picker.Item label="Exterior" value="exterior" />
-          </Picker>
-        </View>
+      <View style={styles.row}>
+        <Text style={styles.inputLabel}>Interior:</Text>
+        <View style={{ width: 65 }} /> 
+        <Picker
+          style={[styles.input, styles.picker]}
+          selectedValue={revisionInterior}
+          onValueChange={(itemValue) => setRevisionInterior(itemValue)}
+        >
+          <Picker.Item label="NO" value="NO" />
+          <Picker.Item label="Visible" value="Visible" />
+          <Picker.Item label="Ingreso" value="Ingreso" />
+        </Picker>
+      </View>
 
-        <View style={styles.inputContainer}>
-          <Text style={styles.inputLabel}>Interior:  </Text>
-          <Picker
-            style={styles.input}
-            selectedValue={revisionInterior}
-            onValueChange={(itemValue) => setRevisionInterior(itemValue)}
-          >
-            <Picker.Item label="Interior" value="interior" />
-          </Picker>
-        </View>      
+      <View style={styles.row}>
+        <Text style={styles.inputLabel}>Revisión planos:</Text>
+        <View style={{ width: 5 }} /> 
+        <Picker
+          style={[styles.input, styles.picker]}
+          selectedValue={revisionPlanos}
+          onValueChange={(itemValue) => setRevisionPlanos(itemValue)}
+        >
+          <Picker.Item label="Sí" value="si" />
+          <Picker.Item label="No" value="no" />
+        </Picker>
+      </View>
 
-
-        <View style={styles.inputContainer}>
-          <Text style={styles.inputLabel}>Revisión planos:  </Text>
-          <Picker
-            style={[styles.input, styles.picker]}
-            selectedValue={revisionPlanos}
-            onValueChange={(itemValue) => setRevisionPlanos(itemValue)}
-          >
-            <Picker.Item label="Rev si" value="si" />
-            <Picker.Item label="Rev No" value="no" />
-          </Picker>
-        </View>
-      
-      <Text style={styles.inputLabel}>Fuente del tipo de suelo  </Text>
-      <TextInput
+      <View style={styles.part1}>
+        <Text style={styles.inputLabel}>Fuente del tipo de suelo:</Text>
+        <TextInput
           style={styles.input}
-          placeholder="Fuente del tipo de suelo"
           value={fuenteTipoSuelo}
           onChangeText={(text) => setFuenteTipoSuelo(text)}
         />
-      <Text style={styles.inputLabel}> </Text>
-      <Text style={styles.inputLabel}>Fuente de peligros geológicos 1  </Text>
-      <TextInput
+      </View>
+
+      <View style={styles.part1}>
+        <Text style={styles.inputLabel}>Fuente de Peligros Geológicos</Text>
+        <TextInput
           style={styles.input}
-          placeholder="Fuente de peligros geológicos 1"
           value={fuentePeligrosGeologicos1}
           onChangeText={(text) => setFuentePeligrosGeologicos1(text)}
         />
+      </View>
 
-      <Text style={styles.inputLabel}> </Text>
-      <Text style={styles.inputLabel}>Fuente de peligros geológicos 2  </Text>
-      <TextInput
+      <View style={styles.part1}>
+        <Text style={styles.inputLabel}>Contacto de la Persona</Text>
+        <TextInput
           style={styles.input}
-          placeholder="Fuente de peligros geológicos 2"
           value={fuentePeligrosGeologicos2}
           onChangeText={(text) => setFuentePeligrosGeologicos2(text)}
         />
+      </View>
 
       <View>
-        <Text style={styles.subtitle}> </Text>
-        <Text style={styles.subtitle}>Otros Peligros</Text>
-
-        <Text style={styles.inputLabelred}>¿Hay peligros que desencadenan una evaluación estructural detallada?</Text>
-   
+        <Text style={[styles.subtitle, styles.centerText]}>Otros Peligros</Text>
+        <Text style={[styles.subtitle, styles.boldRedText, styles.centerText]}>¿Hay peligros que desencadenan una evaluación estructural detallada?</Text>
         <CheckBox
-        º  title="Posible golpeteo entre edificios"
+          title="Posible golpeteo entre edificios"
           checked={checkBox1}
           onPress={() => setCheckBox1(!checkBox1)}
+          containerStyle={styles.transparentCheckBox}
         />
-
         <CheckBox
           title="Riesgo de caída de edificios adyacentes más altos"
           checked={checkBox2}
           onPress={() => setCheckBox2(!checkBox2)}
+          containerStyle={styles.transparentCheckBox}
         />
-
         <CheckBox
           title="Peligro geológico o Suelo tipo F"
           checked={checkBox3}
           onPress={() => setCheckBox3(!checkBox3)}
+          containerStyle={styles.transparentCheckBox}
         />
-
         <CheckBox
           title="Daños significativos/deterioro del sistema estructural"
           checked={checkBox4}
           onPress={() => setCheckBox4(!checkBox4)}
+          containerStyle={styles.transparentCheckBox}
         />
-    </View> 
-      {/* Botones de Navegación */}
-      <View style={styles.buttonContainer}>
-      <TouchableOpacity style={styles.backButton} onPress={() => navigation.goBack()}>
-        <MaterialCommunityIcons name="arrow-left" size={24} color="white" />
-        <Text style={styles.ButtonText}>Regresar</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity style={styles.backButton} onPress={handleNext}>
-        <MaterialCommunityIcons name="arrow-right" size={24} color="white" />
-        <Text style={styles.ButtonText}>Continuar</Text>
-      </TouchableOpacity>
-    </View>
-
-
-   {/*   </View> */}
-    </ScrollView>
+               </View>
+        <View style={{ marginBottom: 50 }}></View> {/* Espacio de 1 centímetro entre los CheckBox y los botones */}
+        
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.prevButton} onPress={() => navigation.goBack()}>
+            <MaterialCommunityIcons name="arrow-left" size={24} color="white" />
+            <Text style={styles.buttonText}>Regresar</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.nextButton} onPress={handleNext}>
+            <Text style={styles.buttonText}>Siguiente</Text>
+            <MaterialCommunityIcons name="arrow-right" size={24} color="white" />
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
   );
 };
 
@@ -227,6 +229,30 @@ const styles = StyleSheet.create({
     flexGrow: 1,
     padding: 16,
     backgroundColor: 'white',
+    paddingBottom: 50, 
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 16,
+  },
+  inputLabel: {
+    fontSize: 16,
+    marginRight: 4,
+    fontWeight: 'normal',
+  },
+  picker: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    justifyContent: 'center',
+    width: 150,
+  },
+  part1: {
+    height: 75,
+    justifyContent: 'center',
+    marginBottom: 16,
   },
   title: {
     fontSize: 24,
@@ -234,51 +260,10 @@ const styles = StyleSheet.create({
     marginBottom: 16,
     textAlign: 'center',
   },
-  section: {
-    marginBottom: 16,
-  },
-  sectionTitle: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 8,
-  },
-  documentButton: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginVertical: 8,
-  },
-  documentButtonText: {
-    color: 'black',
-  },
-  fileName: {
-    marginLeft: 8,
-    fontStyle: 'italic',
-    color: 'gray',
-  },
   subtitle: {
     fontSize: 18,
-    fontWeight: 'bold',
-    //marginBottom: 8,
     marginBottom: 16,
     textAlign: 'center',
-  },
-  inputContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 16,
-  },
-  inputLabel: {
-    fontSize: 16,
-    marginLeft: 8,  
-    fontWeight: 'bold',
-  },
-  inputLabelred: {
-    fontSize: 16,
-    marginLeft: 8,     
-    color: 'red',
   },
   input: {
     flex: 1,
@@ -286,91 +271,59 @@ const styles = StyleSheet.create({
     borderColor: 'gray',
     borderWidth: 1,
     paddingHorizontal: 10,
-    borderRadius: 10, // Ajusta este valor para cambiar la ovalidad
+    borderRadius: 5,
+    marginBottom: 16,
   },
-  backButton: {
-    backgroundColor: 'blue',
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 20, 
+    paddingHorizontal: 16,
+    paddingBottom: 20, 
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: 'white',
+    borderTopColor: 'lightgray',
+  },
+  prevButton: {
+    backgroundColor: 'navy',
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
     paddingVertical: 12,
     borderRadius: 10,
-    marginBottom: 12,
     paddingHorizontal: 24,
-    },
-  backButtonText: {
-    color: 'white',
-    fontSize: 18,
-    marginLeft: 8,
   },
   nextButton: {
-    backgroundColor: 'blue',
-    padding: 12,
-    borderRadius: 10,
+    backgroundColor: 'navy',
+    flexDirection: 'row',
     alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    borderRadius: 10,
+    paddingHorizontal: 24,
   },
-  nextButtonText: {
-    color: 'white',
-    fontSize: 18,
-  },
-  ButtonText: {
+  buttonText: {
     color: 'white',
     fontSize: 18,
     marginLeft: 8,
   },
-  dateInputContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+  transparentCheckBox: {
+    backgroundColor: 'transparent',
+    borderWidth: 0,
   },
-  dateInput: {
-    width: 50,
-    marginRight: 5,
-  },
-  dateSeparator: {
-    fontSize: 20,
-    marginRight: 5,
-  },
-  locationInput: {
-    flex: 1,
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    paddingHorizontal: 10,
-    borderRadius: 10,
-    marginRight: 10,
-  },
-  smallInput: {
-    width: 60,
-    marginRight: 10,
-  },
-  smallInput1: {
-    width: 60,
-    marginRight: 200,
-  },
-  uploadButtonContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between', // Para distribuir los elementos horizontalmente
-  },
-  uploadButton: {
-    backgroundColor: 'blue',
-    borderRadius: 10,
-    paddingHorizontal: 24,
-    paddingVertical: 12,
-  },
-  uploadButtonText: {
-    color: 'white',
-    fontSize: 18,
-  },
-  documentButtonText: {
+  blackText: {
     color: 'black',
   },
-  buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginTop: 16,
+  boldRedText: {
+    color: 'red',
+    fontWeight: 'bold',
   },
-    
+  centerText: {
+    textAlign: 'center',
+  },
 });
 
 export default FormularioFema4;
