d529b107ae7acbbf7b84ca37bdfe40e08b541903
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 <vinculaciongrupofema@gmail.com>
2024-06-23 20:22:50 -0500

    Add formulario fema 5

--- frontvinculacion/src/components/FormularioFema5.js
+++ frontvinculacion/src/components/FormularioFema5.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, ScrollView, TouchableOpacity,Alert } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, TouchableOpacity, Alert } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 import { CheckBox } from 'react-native-elements';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const FormularioFema5 = ({ route, navigation }) => {
+  
   // Obtener datos de las pantallas anteriores
   const { params } = route;
   const {
@@ -56,6 +57,10 @@ const FormularioFema5 = ({ route, navigation }) => {
   const [checkBox2, setCheckBox2] = useState(false);
   const [checkBox3, setCheckBox3] = useState(false);
   const [checkBox4, setCheckBox4] = useState(false);
+  const [checkBox5, setCheckBox5] = useState(false);
+  const [checkBox6, setCheckBox6] = useState(false);
+  const [checkBox7, setCheckBox7] = useState(false);
+  const [checkBox8, setCheckBox8] = useState(false);
   // Estados para los campos del FormularioParte5
   const [accionRequerida, setAccionRequerida] = useState('');
   const [evaluacionDetalladaElementosNoEstructurales, setEvaluacionDetalladaElementosNoEstructurales] = useState('');
@@ -66,65 +71,13 @@ const FormularioFema5 = ({ route, navigation }) => {
       "¡Formulario guardado con éxito!",
       "",
       [
-          {
-              text: "Ok",
-              onPress: () => navigation.navigate('Dashboard')
-          }
+        {
+          text: "Ok",
+          onPress: () => navigation.navigate('Dashboard')
+        }
       ],
       { cancelable: false }
-  );
-
-    /*navigation.navigate('FormularioGuardado', {
-      direccion,
-      zip,
-      otrasIdentificaciones,
-      nombreEdificio,
-      uso,
-      latitud,
-      longitud,
-      inspector,
-      fecha,
-      hora,
-      files1,
-      files2,
-      numPisos,
-      sup,
-      info,
-      anioConstruccion,
-      areaTotalPiso,
-      anioCodigo,
-      anioConstruccion2,
-      ampliacion,
-      ocupacion,
-      tipoSuelo,
-      comentario,
-      tipoIdentificacionDNK,
-      resultadoBase,
-      irregularidadVerticalSevera,
-      irregularidadVerticalModerada,
-      plantaIrregular,
-      preCodigoSismico,
-      postCodigoSismico,
-      sueloTipoAoB,
-      sueloTipoE1_3Pisos,
-      sueloTipoE_GT3Pisos,
-      resultadoMinimoSmin,
-      resultadoFinalSL1_GT_Smin,
-      revisionExterior,
-      revisionInterior,
-      revisionPlanos,
-      fuenteTipoSuelo,
-      fuentePeligrosGeologicos1,
-      fuentePeligrosGeologicos2,
-      evaluacionDetallada,
-      checkBox1,
-      checkBox2,
-      checkBox3,
-      checkBox4,
-      accionRequerida,
-      evaluacionDetalladaElementosNoEstructurales,
-      inspeccionNivel2,
-    });*/
+    );
   };
 
   return (
@@ -132,60 +85,96 @@ const FormularioFema5 = ({ route, navigation }) => {
       <Text style={styles.title}>Formulario FEMA P-154</Text>
       <Text style={styles.subtitle}>Acción requerida</Text>
 
-      <Text>¿Se requiere de una evaluación estructural más detallada?</Text>
+      <Text style={[styles.textoRojo, styles.subtitle1]}>
+       ¿Se requiere de una evaluación estructural más detallada?
+      </Text>
       <CheckBox
         title="Si, se desconoce el tipo de edificio según FEMA"
         checked={checkBox1}
         onPress={() => setCheckBox1(!checkBox1)}
+        containerStyle={styles.checkboxContainer}
+        textStyle={styles.checkboxText}
       />
       <CheckBox
         title="Si, resultado menor que el límite"
         checked={checkBox2}
         onPress={() => setCheckBox2(!checkBox2)}
+        containerStyle={styles.checkboxContainer}
+        textStyle={styles.checkboxText}
       />
       <CheckBox
         title="Si, otros peligros presentes"
         checked={checkBox3}
         onPress={() => setCheckBox3(!checkBox3)}
+        containerStyle={styles.checkboxContainer}
+        textStyle={styles.checkboxText}
       />
       <CheckBox
         title="No"
         checked={checkBox4}
         onPress={() => setCheckBox4(!checkBox4)}
+        containerStyle={styles.checkboxContainer}
+        textStyle={styles.checkboxText}
       />
-      <Text style={styles.inputLabelred}>¿Se requiere una evaluación detallada de elementos no estructurales?</Text>
-      <Picker
-        style={styles.input}
-        selectedValue={evaluacionDetalladaElementosNoEstructurales}
-        onValueChange={(itemValue) => setEvaluacionDetalladaElementosNoEstructurales(itemValue)}
-      >
-        <Picker.Item label="Si, hay peligro de caída de elementos" value="siPeligroCaidaElementos" />
-        <Picker.Item label="No, existe amenaza de elementos no estructurales y deben ser mitigados, pero la evaluación detallada no es necesaria" value="noAmenazaMitigada" />
-        <Picker.Item label="No, no existe peligro de elementos no estructurales" value="noPeligroElementos" />
-        <Picker.Item label="No, se sabe" value="noSabe" />
-      </Picker>
-     
-      <Text style={styles.inputLabelred}>¿Se requiere de una inspección de Nivel 2?</Text>      
-      <Picker
-        style={styles.input}
-        selectedValue={inspeccionNivel2}
-        onValueChange={(itemValue) => setInspeccionNivel2(itemValue)}
-      >
-        <Picker.Item label="Si" value="si" />
-        <Picker.Item label="No" value="no" />
-      </Picker>
+
+      <Text style={[styles.textoRojo, styles.subtitle1]}>
+         ¿Se requiere una evaluación detallada de elementos no estructurales?
+      </Text>
+      <CheckBox
+        title="Si, hay peligro de caída de elementos"
+        checked={checkBox5}
+        onPress={() => setCheckBox5(!checkBox5)}
+        containerStyle={styles.checkboxContainer}
+        textStyle={styles.checkboxText}
+      />
+      <CheckBox
+        title="No, existe amenaza de elementos no estructurales y deben ser mitigados, pero la evaluación detallada no es necesaria"
+        checked={checkBox6}
+        onPress={() => setCheckBox6(!checkBox6)}
+        containerStyle={styles.checkboxContainer}
+        textStyle={styles.checkboxText}
+      />
+      <CheckBox
+        title="No, no existe peligro de elementos no estructurales"
+        checked={checkBox7}
+        onPress={() => setCheckBox7(!checkBox7)}
+        containerStyle={styles.checkboxContainer}
+        textStyle={styles.checkboxText}
+      />
+      <CheckBox
+        title="No, se sabe"
+        checked={checkBox8}
+        onPress={() => setCheckBox8(!checkBox8)}
+        containerStyle={styles.checkboxContainer}
+        textStyle={styles.checkboxText}
+      />
+
+<View style={styles.inline}>
+   <Text style={[styles.textoRojo, styles.subtitle1]}>
+      ¿Se requiere de una inspección de Nivel 2?
+  </Text>
+  <Picker
+    style={[styles.input, styles.pickerSmall]}
+    selectedValue={inspeccionNivel2}
+    onValueChange={(itemValue) => setInspeccionNivel2(itemValue)}
+  >
+    <Picker.Item label="Si" value="si" />
+    <Picker.Item label="No" value="no" />
+  </Picker>
+</View>
+
 
       {/* Botones de Navegación */}
       <View style={styles.buttonContainer}>
-      <TouchableOpacity style={styles.backButton} onPress={() => navigation.goBack()}>
-        <MaterialCommunityIcons name="arrow-left" size={24} color="white" />
-        <Text style={styles.ButtonText}>Regresar</Text>
-      </TouchableOpacity>
-
-        <TouchableOpacity style={styles.guardarButton} onPress={handleGuardar}>
-          <Text style={styles.ButtonText}>Guardar</Text>
+        <TouchableOpacity style={[styles.button, { backgroundColor: 'navy' }]} onPress={() => navigation.goBack()}>
+          <MaterialCommunityIcons name="arrow-left" size={24} color="white" />
+          <Text style={styles.buttonText}>Regresar</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.button, { backgroundColor: 'navy' }]} onPress={handleGuardar}>
+          <Text style={styles.buttonText}>Guardar</Text>
         </TouchableOpacity>
       </View>
+      <View style={{ marginBottom: 10 }}></View> {/* Espacio de 1 centímetro antes de finalizar la página */}
     </ScrollView>
   );
 };
@@ -194,7 +183,7 @@ const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
     padding: 16,
-    backgroundColor: 'white',   
+    backgroundColor: 'white',
   },
   title: {
     fontSize: 24,
@@ -204,9 +193,15 @@ const styles = StyleSheet.create({
   },
   subtitle: {
     fontSize: 18,
-    fontWeight: 'bold',
     marginBottom: 16,
     textAlign: 'center',
+    fontWeight: 'bold',
+    marginBottom: 10, // Ajusta según sea necesario
+  },
+  subtitle1: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 8, // Ajusta según sea necesario
   },
   input: {
     height: 40,
@@ -215,73 +210,53 @@ const styles = StyleSheet.create({
     marginBottom: 16,
     paddingHorizontal: 10,
   },
-  inputLabelred: {
-    fontSize: 16,
-    marginLeft: 8,     
-    color: 'red',
-  },
   buttonContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     marginTop: 16,
   },
-  prevButton: {
+  button: {
     flex: 1,
-    alignItems: 'center',
+    flexDirection: 'row',
+    alignItems: 'center'
+    ,
     justifyContent: 'center',
     height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
     marginHorizontal: 8,
-    cursor: 'pointer',
-    backgroundColor: 'lightgray',
-  },
-//  guardarButton: {
-//    flex: 1,
-//    alignItems: 'center',
-//    justifyContent: 'center',
-//    height: 40,
-//    borderColor: 'gray',
-//    borderWidth: 1,
-//    marginHorizontal: 8,
-//    cursor: 'pointer',
-//    backgroundColor: 'green',
-///  },
-
-  nextButtonText: {
-    fontSize: 20,
-  },
-  backButton: {
-    backgroundColor: 'blue',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingVertical: 12,
+    backgroundColor: 'navy',
     borderRadius: 10,
-    marginBottom: 12,
-    paddingHorizontal: 24,
-    },
-    guardarButton: {
-      backgroundColor: 'blue',
-      flexDirection: 'row',
-      alignItems: 'center',
-      justifyContent: 'center',
-      paddingVertical: 12,
-      borderRadius: 10,
-      marginBottom: 12,
-      paddingHorizontal: 24,
-      },
-  
-  backButtonText: {
-    color: 'white',
+    paddingHorizontal: 12,
+  },
+  buttonText: {
     fontSize: 18,
+    color: 'white',
     marginLeft: 8,
   },
-  ButtonText: {
-    color: 'white',
-    fontSize: 18,
+  checkboxContainer: {
+    backgroundColor: 'transparent',
+    borderWidth: 0,
+    marginBottom: 8,
+  },
+  checkboxText: {
+    fontSize: 16,
     marginLeft: 8,
-  },    
+  },
+  inline: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 8,
+  },
+  inlineLabel: {
+    fontSize: 16,
+  },
+  pickerSmall: {
+    width: 60, // Ajusta el ancho según sea necesario
+    borderRadius: 8, // Ajusta el radio de borde según sea necesario
+  },
+  textoRojo: {
+    color: 'red',
+  },  
 });
 
 export default FormularioFema5;
