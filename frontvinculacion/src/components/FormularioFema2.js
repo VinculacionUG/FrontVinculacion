568a71c6efd06efdf7172929efd7e411f881ffe4
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 <vinculaciongrupofema@gmail.com>
2024-07-09 10:36:13 -0500

    add diff checkbox and view consle result

--- frontvinculacion/src/components/FormularioFema2.js
+++ frontvinculacion/src/components/FormularioFema2.js
@@ -16,7 +16,7 @@ const FormularioFema2 = ({ route, navigation }) => {
   const [ocupacion, setOcupacion] = useState([]);
   const [tipoSuelo, setTipoSuelo] = useState([]);
   const [comentario, setComentario] = useState('');
-  //const [checkBox1, setCheckBox1] = useState(false);
+  //const [checkBox1, setCheckBox1] = useState(false);  
   //const [checkBox2, setCheckBox2] = useState(false);
   //const [checkBox3, setCheckBox3] = useState(false);
   //const [checkBox4, setCheckBox4] = useState(false);
@@ -25,8 +25,8 @@ const FormularioFema2 = ({ route, navigation }) => {
   //const [checkBox7, setCheckBox7] = useState(false);
   //const [checkBox8, setCheckBox8] = useState(false);
   //const [checkBox9, setCheckBox9] = useState(false);
-  const [tipoocupacion, setTipoOcupacion] = useState([]);  
-  
+  const [tipoocupacion, setTipoOcupacion] = useState([]);
+
   const { params } = route;
   const {
     direccion,
@@ -64,27 +64,37 @@ const FormularioFema2 = ({ route, navigation }) => {
       anioCodigo,
       anioConstruccion2,
       ampliacion,
-      ocupacion,
+      ocupacion: selectedCheckboxes,
       tipoSuelo,
       tipoocupacion,
       comentario,
-  //    checkBox1,
-  //    checkBox2,
-  //    checkBox3,
-  //    checkBox4,
-  //    checkBox5,
-  //    checkBox6,
-  //    checkBox7,
-  //    checkBox8,
-  //    checkBox9,
+      //    checkBox1,
+      //    checkBox2,
+      //    checkBox3,
+      //    checkBox4,
+      //    checkBox5,
+      //    checkBox6,
+      //    checkBox7,
+      //    checkBox8,
+      //    checkBox9,
     });
   };
 
-    const [selectedCheckbox, setSelectedCheckbox] = useState(null);
-    const handleCheckboxChange = (codOcupacion) => {
-      setSelectedCheckbox(codOcupacion);
-    };
- 
+  const [selectedCheckboxes, setSelectedCheckboxes] = useState([]);
+
+  const handleCheckboxChange = (codOcupacion) => {
+    setSelectedCheckboxes(prevState => {
+      const updatedCheckboxes = prevState.includes(codOcupacion)
+        ? prevState.filter(item => item !== codOcupacion)
+        : [...prevState, codOcupacion];
+
+      // Mostrar IDs seleccionados en la consola
+      console.log('Selected IDs:', updatedCheckboxes);
+
+      return updatedCheckboxes;
+    });
+  };
+
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [selectedValue, setSelectedValue] = useState('');
@@ -94,49 +104,49 @@ const FormularioFema2 = ({ route, navigation }) => {
   useEffect(() => {
     // URL del servicio GET
     //const url = 'http://localhost:3000/api/TipoOcupacion';
-    const url = 'https://www.fema.somee.com/Users/TipoOcupacion';   
+    const url = 'https://www.fema.somee.com/Users/TipoOcupacion';
     const fetchTipoOcupacion = async () => {
       try {
         const response = await fetch(url,
-		{
-			method: 'GET',
-		}
-		);
+          {
+            method: 'GET',
+          }
+        );
         if (!response.ok) {
           throw new Error('Error en la red');
         }
         const result = await response.json();
         setTipoOcupacion(result);
-		    //console.log(result);    
+        //console.log(result);    
       } catch (error) {
         setError(error);
-		console.log(error);    
+        console.log(error);
       } finally {
         setLoading(false);
       }
     };
     fetchTipoOcupacion();
 
-    
+
     // URL del servicio GET
     //const url2 = 'http://localhost:3001/api/TipoSuelo';
     const url2 = 'https://www.fema.somee.com/Users/TipoSuelo';
     const fetchTipoSuelo = async () => {
       try {
         const response = await fetch(url2,
-		{
-			method: 'GET',
-		}
-		);
+          {
+            method: 'GET',
+          }
+        );
         if (!response.ok) {
           throw new Error('Error en la red');
         }
         const result = await response.json();
         setTipoSuelo(result);
-		//console.log(result);    
+        //console.log(result);    
       } catch (error) {
         setError(error);
-		console.log(error);    
+        console.log(error);
       } finally {
         setLoading(false);
       }
@@ -150,21 +160,21 @@ const FormularioFema2 = ({ route, navigation }) => {
     const fetchOcupacion = async () => {
       try {
         const response = await fetch(url3,
-		{
-			method: 'GET',
-		}
-		);
+          {
+            method: 'GET',
+          }
+        );
         if (!response.ok) {
           throw new Error('Error en la red');
         }
         const result = await response.json();
         setOcupacion(result);
-		//console.log(result);    
-    //console.log(ocupacion);    
+        //console.log(result);    
+        //console.log(ocupacion);    
 
       } catch (error) {
         setError(error);
-		console.log(error);    
+        console.log(error);
       } finally {
         setLoading(false);
       }
@@ -189,6 +199,9 @@ const FormularioFema2 = ({ route, navigation }) => {
     );
   }
 
+  const selectedDescriptions = ocupacion
+    .filter(checkbox => selectedCheckboxes.includes(checkbox.codOcupacion))
+    .map(checkbox => checkbox.descripcion);
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
@@ -204,7 +217,7 @@ const FormularioFema2 = ({ route, navigation }) => {
             <Picker.Item key={i + 1} label={`${i + 1}`} value={`${i + 1}`} />
           ))}
         </Picker>
-        
+
         <Text style={[styles.inputLabel, styles.infLabel]}>Inf:</Text>
         <Picker
           style={styles.smallPicker}
@@ -216,10 +229,10 @@ const FormularioFema2 = ({ route, navigation }) => {
           ))}
         </Picker>
       </View>
-          
+
       <View style={styles.inputContainer}>
         <Text style={[styles.inputLabel, { height: 40, marginRight: 2 }]}>Año Construcción:</Text>
-        <View style={{ width: 5, height: 40 }} /> 
+        <View style={{ width: 5, height: 40 }} />
         <TextInput
           style={[styles.input, { width: 50, height: 40 }]}
           value={anioConstruccion}
@@ -227,7 +240,7 @@ const FormularioFema2 = ({ route, navigation }) => {
           onChangeText={(text) => {
             const numericValue = text.replace(/[^0-9]/g, '');
             setAnioConstruccion(numericValue);
-           }}
+          }}
         />
         <View style={{ width: 10, height: 40 }} />
         <Text style={[styles.inputLabel, { height: 40, width: 150 }]}>Área total de piso (m2):</Text>
@@ -244,9 +257,9 @@ const FormularioFema2 = ({ route, navigation }) => {
 
       <View style={styles.inputContainer}>
         <Text style={styles.inputLabel}>Año de código:</Text>
-        <View style={{ width: 5 }} /> 
+        <View style={{ width: 5 }} />
         <TextInput
-          style={[styles.input, { width: 0 }]} 
+          style={[styles.input, { width: 0 }]}
           value={anioCodigo}
           onChangeText={(text) => {
             const numericValue = text.replace(/[^0-9]/g, '');
@@ -266,8 +279,8 @@ const FormularioFema2 = ({ route, navigation }) => {
       </View>
 
       <View style={styles.inputContainer}>
-        <Text style={styles.inputLabel }>Año de Construcción:</Text>
-       
+        <Text style={styles.inputLabel}>Año de Construcción:</Text>
+
         <TextInput
           //style={[styles.input, { width: 20, height: 20 }]}
           style={styles.inputText}
@@ -279,209 +292,212 @@ const FormularioFema2 = ({ route, navigation }) => {
           maxLength={4}
         />
       </View>
- {/*   */}
+      {/*   */}
 
-    <Text style={[styles.subtitle, styles.centerText]}>Ocupación:</Text>
-    <Text style={[styles.subtitle, styles.boldRedText, styles.centerText]}></Text>
- 
-    <View style={styles.checkboxGrid}>
+      <Text style={[styles.subtitle, styles.centerText]}>Ocupación:</Text>
+      <Text style={[styles.subtitle, styles.boldRedText, styles.centerText]}></Text>
+
+      <View style={styles.checkboxGrid}>
         {ocupacion.map((checkbox) => (
           <View key={checkbox.codOcupacion} style={styles.checkboxContainer}>
             <CheckBox
               title={checkbox.descripcion}
-              checked={selectedCheckbox === checkbox.codOcupacion}
+              checked={selectedCheckboxes.includes(checkbox.codOcupacion)}
               onPress={() => handleCheckboxChange(checkbox.codOcupacion)}
               containerStyle={styles.checkbox}
+              textStyle={styles.checkboxLabel}
+              checkedIcon={<MaterialCommunityIcons name="checkbox-marked" size={24} color="green" />}
+              uncheckedIcon={<MaterialCommunityIcons name="checkbox-blank-outline" size={24} color="gray" />}
             />
           </View>
         ))}
       </View>
 
-      {selectedCheckbox !== null && (
+      {selectedCheckboxes !== null && (
         <Text style={styles.resultado}>
-          Seleccionaste: {ocupacion.find(checkbox => checkbox.codOcupacion === selectedCheckbox).descripcion}
+          Seleccionaste: {selectedDescriptions.join(', ')}
         </Text>
       )}
 
-    <View style={styles.inputContainer}>
-      <Text style={styles.inputLabel}>Tipo de Ocupación:</Text>
-      <View style={{ width: 5, height: 40 }} /> 
-      <Picker
-        style={styles.smallPicker}
-        selectedValue={selectedValuetipoocupacion}
-        onValueChange={(itemValue) => setSelectedValueTipoOcupacion(itemValue)}
-      >
-        {tipoocupacion.map((item, index) => (
-          <Picker.Item label={item.descripcion} value={item.descripcion} key={index} />
-        ))}
-      </Picker>
-      {/*  <Text style={styles.selected}>Seleccionado: {selectedValue}</Text> */}
-    </View>
+      <View style={styles.inputContainer}>
+        <Text style={styles.inputLabel}>Tipo de Ocupación:</Text>
+        <View style={{ width: 5, height: 40 }} />
+        <Picker
+          style={styles.smallPicker}
+          selectedValue={selectedValuetipoocupacion}
+          onValueChange={(itemValue) => setSelectedValueTipoOcupacion(itemValue)}
+        >
+          {tipoocupacion.map((item, index) => (
+            <Picker.Item label={item.descripcion} value={item.descripcion} key={index} />
+          ))}
+        </Picker>
+        {/*  <Text style={styles.selected}>Seleccionado: {selectedValue}</Text> */}
+      </View>
 
 
-    <View style={styles.inputContainer}>
-      <Text style={styles.inputLabel}>Tipo de Suelo:</Text>
-      <View style={{ width: 5, height: 40 }} /> 
-      <Picker
-        style={styles.smallPicker}
-        selectedValue={selectedValuetipoSuelo}
-        onValueChange={(itemValue) => setSelectedValueTipoSuelo(itemValue)}
-      >
-        {tipoSuelo.map((item, index) => (
-          <Picker.Item label={item.descripcion} value={item.descripcion} key={index} />
-        ))}
-      </Picker>
-      {/*  <Text style={styles.selected}>Seleccionado: {selectedValue}</Text> */}
-    </View>
+      <View style={styles.inputContainer}>
+        <Text style={styles.inputLabel}>Tipo de Suelo:</Text>
+        <View style={{ width: 5, height: 40 }} />
+        <Picker
+          style={styles.smallPicker}
+          selectedValue={selectedValuetipoSuelo}
+          onValueChange={(itemValue) => setSelectedValueTipoSuelo(itemValue)}
+        >
+          {tipoSuelo.map((item, index) => (
+            <Picker.Item label={item.descripcion} value={item.descripcion} key={index} />
+          ))}
+        </Picker>
+        {/*  <Text style={styles.selected}>Seleccionado: {selectedValue}</Text> */}
+      </View>
 
-        <View style={styles.inputContainer}>
-  <Text style={styles.inputLabel}>Comentario:</Text>
-</View>
-<View style={styles.inputContainer}>
-  <TextInput
-    style={[styles.input, styles.multilineText, { width: '100%' }]}
-    multiline
-    numberOfLines={4}
-    value={comentario}
-    onChangeText={(text) => setComentario(text)}
-  />
-</View>
-        
-        <TouchableOpacity style={styles.backButton} onPress={() => navigation.goBack()}>
-          <MaterialCommunityIcons name="arrow-left" size={24} color="white" />
-          <Text style={styles.buttonText}>Regresar</Text>
-        </TouchableOpacity>
-        
-        <TouchableOpacity style={styles.nextButton} onPress={handleNext}>
-          <MaterialCommunityIcons name="arrow-right" size={24} color="white" />
-          <Text style={styles.buttonText}>Siguiente</Text>
-        </TouchableOpacity>
-        
-      </ScrollView>
-    );
-  };
+      <View style={styles.inputContainer}>
+        <Text style={styles.inputLabel}>Comentario:</Text>
+      </View>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={[styles.input, styles.multilineText, { width: '100%' }]}
+          multiline
+          numberOfLines={4}
+          value={comentario}
+          onChangeText={(text) => setComentario(text)}
+        />
+      </View>
+
+      <TouchableOpacity style={styles.backButton} onPress={() => navigation.goBack()}>
+        <MaterialCommunityIcons name="arrow-left" size={24} color="white" />
+        <Text style={styles.buttonText}>Regresar</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.nextButton} onPress={handleNext}>
+        <MaterialCommunityIcons name="arrow-right" size={24} color="white" />
+        <Text style={styles.buttonText}>Siguiente</Text>
+      </TouchableOpacity>
+
+    </ScrollView>
+  );
+};
 
 
- 
-  const styles = StyleSheet.create({
-    container: {
-      flexGrow: 1,
-      padding: 16,
-      backgroundColor: 'white',
-      paddingBottom: 100,
-    },
-    title: {
-      fontSize: 24,
-      fontWeight: 'bold',
-      marginBottom: 16,
-      textAlign: 'center',
-    },
-    inputContainer: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      marginBottom: 16,
-    },
-    inputLabel: {
-      fontSize: 16,
-      marginRight: 4, // Espacio de 4 píxeles para los 2 mm
-      fontWeight: 'normal',
-    },
-    infLabel: {
-      marginLeft: 20, // Espacio de 4 píxeles entre el primer selector y el texto "Inf:"
-    },
-    input: {
-      flex: 1,
-      height: 40,
-      borderColor: 'gray',
-      borderWidth: 1,
-      paddingHorizontal: 10,
-      borderRadius: 5,
-    },
-    smallPicker: {
-      flex: 1,
-      height: 40, // Reducir altura del selector
-      borderColor: 'gray',
-      borderWidth: 1,
-      borderRadius: 5,
-      justifyContent: 'center',
-      width: 80, // Reducir el ancho del selector
-    },
-    backButton: {
-      backgroundColor: 'navy',
-      flexDirection: 'row',
-      alignItems: 'center',
-      justifyContent: 'center',
-      paddingVertical: 12,
-      borderRadius: 10,
-      paddingHorizontal: 24,
-      position: 'absolute',
-      bottom: 16,
-      left: 16,
-    },
-    nextButton: {
-      backgroundColor: 'navy',
-      flexDirection: 'row',
-      alignItems: 'center',
-      justifyContent: 'center',
-      paddingVertical: 12,
-      borderRadius: 10,
-      paddingHorizontal: 24,
-      position: 'absolute',
-      bottom: 16,
-      right: 16,
-    },
-    buttonText: {
-      color: 'white',
-      fontSize: 18,
-      marginLeft: 8,
-    },
-    multilineText: {
-      minHeight: 100,
-    },
-    CheckBoxContainer: {
-      flexGrow: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      paddingHorizontal: 0,
-      paddingVertical: 20,
-    },
-    checkboxGrid: {
-      flexDirection: 'row',
-      flexWrap: 'wrap',
-      paddingHorizontal: 0,
-      //justifyContent: 'space-between',
-    },
-    checkboxContainer: {
-      width: '30%',
-      padding: 0,
-    },
-    checkbox: {
-      backgroundColor: 'transparent',
-      borderWidth: 0,
-    },
-    button: {
-      marginTop: 20,
-      width: '100%',
-      backgroundColor: '#2196F3',
-    },
-    resultado: {
-      marginTop: 20,
-      fontSize: 18,
-      color: 'green',
-    },
-    inputText: {
-      flex: .20,
-      flexDirection: 'row',
-      borderWidth: 1,
-      borderColor: 'gray',
-      borderRadius: 5,
-      height: 40,
-      width: 0,
-      padding: 0,
-      paddingHorizontal: 10,
-      marginRight: 2,
-      //backgroundColor: 'red',
-    }, 
-  });
-  
-  export default FormularioFema2;
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    padding: 16,
+    backgroundColor: 'white',
+    paddingBottom: 100,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+    textAlign: 'center',
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 16,
+  },
+  inputLabel: {
+    fontSize: 16,
+    marginRight: 4, // Espacio de 4 píxeles para los 2 mm
+    fontWeight: 'normal',
+  },
+  infLabel: {
+    marginLeft: 20, // Espacio de 4 píxeles entre el primer selector y el texto "Inf:"
+  },
+  input: {
+    flex: 1,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+  },
+  smallPicker: {
+    flex: 1,
+    height: 40, // Reducir altura del selector
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    justifyContent: 'center',
+    width: 80, // Reducir el ancho del selector
+  },
+  backButton: {
+    backgroundColor: 'navy',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    borderRadius: 10,
+    paddingHorizontal: 24,
+    position: 'absolute',
+    bottom: 16,
+    left: 16,
+  },
+  nextButton: {
+    backgroundColor: 'navy',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    borderRadius: 10,
+    paddingHorizontal: 24,
+    position: 'absolute',
+    bottom: 16,
+    right: 16,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+    marginLeft: 8,
+  },
+  multilineText: {
+    minHeight: 100,
+  },
+  CheckBoxContainer: {
+    flexGrow: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 0,
+    paddingVertical: 20,
+  },
+  checkboxGrid: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    paddingHorizontal: 0,
+    //justifyContent: 'space-between',
+  },
+  checkboxContainer: {
+    width: '30%',
+    padding: 0,
+  },
+  checkbox: {
+    backgroundColor: 'transparent',
+    borderWidth: 0,
+  },
+  button: {
+    marginTop: 20,
+    width: '100%',
+    backgroundColor: '#2196F3',
+  },
+  resultado: {
+    marginTop: 20,
+    fontSize: 18,
+    color: 'green',
+  },
+  inputText: {
+    flex: .20,
+    flexDirection: 'row',
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 5,
+    height: 40,
+    width: 0,
+    padding: 0,
+    paddingHorizontal: 10,
+    marginRight: 2,
+    //backgroundColor: 'red',
+  },
+});
+
+export default FormularioFema2;
